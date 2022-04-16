ac3c3ff4f56ee4404989091a90aaedcc805d7450
DanB1998 <danbeasley1998@yahoo.co.uk>
2022-04-16 13:21:13 +0000

    Docstrings and comments tidied up

--- run.py
+++ run.py
@@ -7,45 +7,53 @@ hits = 0
 turns = 0
 
 
-# Making the game board
 def make_game_board(board):
+    """
+    Making the game board
+    """
     for x in range(board_length):
         board.append(' O ' * board_length)
 
 
-# Showing the game board
-def print_game_board(board):
+def print_game_board():
+    """
+    Showing the game board
+    """
     for x in board:
         print(x)
 
 
-# Function to get a random column for a ship
 def rand_row(board):
+    """
+    Function to get a random column for a ship
+    """
     return randint(0, board_length-1)
 
 
-# Function to get a random column for a ship
 def rand_col(board):
+    """
+    Function to get a random column for a ship
+    """
     return randint(0, board_length-1)
 
 
-# Plants the ships
-def plant_ships():
+def plant_ships(board):
+    """
+    Plants the ships
+    """
     global ship1_pos
     ship1_pos = [rand_row(board), rand_col(board)]
     print(ship1_pos)
 
 
-# Game Logic
 def run_game():
     """
     Function that will run the game
     """
-    print_game_board(board)
+    print_game_board()
     guess()
 
 
-# User guess
 def guess():
     """
     Takes a guess from a user
@@ -66,7 +74,6 @@ def guess():
         guess()
 
 
-# Input validation
 def validate_input(coordinates):
     """
     A function that will validate the user's
@@ -88,19 +95,23 @@ def validate_input(coordinates):
     return True
 
 
-# Checks for hits when data is validated
 def check_for_hit():
+    """
+    Checks for hits when data is validated
+    """
     if (ship1_pos[1] == int(user_entry_col) and ship1_pos[0] == int(user_entry_row)):
         print("You hit my battleship")
     else:
-        print("You missed my battleship")    
+        print("You missed my battleship")  
 
 
-# Main functions
 def main():
+    """
+    Main functions
+    """
     print("Welcome to Python Battleships")
     make_game_board(board)
-    plant_ships()
+    plant_ships(board)
     run_game()
 
 
