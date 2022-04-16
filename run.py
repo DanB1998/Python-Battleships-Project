f7dc94017fd7e89b338b515c8c9b1c1092d6aa76
DanB1998 <danbeasley1998@yahoo.co.uk>
2022-04-16 19:59:43 +0000

    Update hit, update miss and arguments

--- run.py
+++ run.py
@@ -24,12 +24,12 @@ def print_game_board(board):
         print("".join(row))
 
 
-def plant_ships(board):
+def plant_ships_computer():
     """
     Plants the ships
     """
-    global ship1_pos
-    ship1_pos = [randint(0, board_length), randint(0, board_length)]
+    global comp_ship1_pos
+    comp_ship1_pos = [randint(0, board_length), randint(0, board_length)]
 
 
 def run_game():
@@ -95,26 +95,33 @@ def check_for_hit():
     if (ship1_pos[1] == user_entry_col and ship1_pos[0] == user_entry_row):
         hits += 1
         print("You hit a battleship \n")
-        update_hit = player_board[user_entry_col].split()
-        update_hit[user_entry_row] = "#"
-        updated_row = " ".join(update_hit)
-        player_board[user_entry_col] = updated_row
+        update_board_hit(player_board)
         print_game_board(player_board)
         check_win()
     else:
         print("You missed my battleship \n")
-        update_miss = player_board[user_entry_col].split()
-        update_miss[user_entry_row] = "X"
-        updated_row = " ".join(update_miss)
-        player_board[user_entry_col] = updated_row
         print_game_board(player_board)
         guess()
 
 
-def update_board():
+def update_board_hit(board):
     """
     Will update the board with a hit
     """
+    update_hit = board[user_entry_col].split()
+    update_hit[user_entry_row] = "#"
+    updated_row = " ".join(update_hit)
+    board[user_entry_col] = updated_row
+
+
+def update_board_miss(board):
+    """
+    Will update the board with a hit
+    """
+    update_miss = board[user_entry_col].split()
+    update_miss[user_entry_row] = "X"
+    updated_row = " ".join(update_miss)
+    board[user_entry_col] = updated_row
 
 
 def check_win():
